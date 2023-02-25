9ac1087612840c2e414d5cf7c3f3c23ab877c3ef
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-02-25 13:12:13 +0100

    Update dumper.js

--- dumper.js
+++ dumper.js
@@ -1,20 +1,36 @@
-let args = process.argv.toString().split(',')
+const { Relay } = require('bedrock-protocol');
+const fs = require('fs');
+const path = require('path');
 
-const { Relay } = require('bedrock-protocol')
+const args = process.argv.slice(2);
+
 const relay = new Relay({
-  version: args[2],
-  host: args[3],
-  port: parseInt(args[4]),
+  version: args[0],
+  host: args[1],
+  port: parseInt(args[2]),
   destination: {
-    host: args[5],
-    port: parseInt(args[6]),
+    host: args[3],
+    port: parseInt(args[4]),
   }
-})
-relay.listen()
+});
+
+relay.listen();
 
 relay.on('connect', player => {
   player.on('clientbound', ({ name, params }) => {
-    if (name === 'move_entity' || name === 'set_entity_data' || name === 'level_event') return
-    console.log('[info] ' + name + ' ' + '%o', params)
-  })
-})
+    if (name === 'move_entity' || name === 'set_entity_data' || name === 'level_event') return;
+    const packet = {
+      name,
+      params,
+    };
+    const packetJson = JSON.stringify(packet);
+    const timestamp = Date.now();
+    const filename = `${name}_${timestamp}.json`;
+    const filePath = path.join(__dirname, filename);
+    fs.writeFile(filePath, packetJson, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+});
