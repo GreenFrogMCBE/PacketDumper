8db01f85489f695d948fc9af83584023951e1dba
kotinash <92477814+kotinash@users.noreply.github.com>
2024-02-17 15:11:57 +0100

    Dummy commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const { Relay } = require('frog-protocol');
+const { Relay } = require('bedrock-protocol');
 const fs = require('fs');
 const path = require('path');
 
 const args = process.argv.slice(2);
-
-fs.mkdirSync("packets", { recursive: true })
 
 const relay = new Relay({
   version: args[0],
@@ -20,22 +18,20 @@ relay.listen();
 
 relay.on('connect', player => {
   player.on('clientbound', ({ name, params }) => {
+    if (name === 'move_entity' || name === 'set_entity_data' || name === 'level_event') return;
+
     const packet = {
       name,
       params,
     };
-
-    let packetJson
-    try {
-      packetJson = JSON.stringify(packet);
-    } catch (error) {
-      return;
-    }
+    const packetJson = JSON.stringify(packet);
     const timestamp = Date.now();
     const filename = `${name}_${timestamp}.json`;
-    const filePath = path.join(__dirname, "packets", filename);
-
-    fs.writeFileSync(filePath, packetJson);
-    console.log('%o', packetJson)
+    const filePath = path.join(__dirname, filename);
+    fs.writeFile(filePath, packetJson, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
   });
 });
