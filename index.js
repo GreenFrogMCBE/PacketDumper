ee4ad6e7dd267d30541ce3817fee1b4a35e02fe0
kotinash <92477814+kotinash@users.noreply.github.com>
2024-03-17 13:15:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const { Relay } = require('frog-protocol');
-const fs = require('fs');
-const path = require('path');
+const { Relay } = require('frog-protocol')
+const fs = require('fs')
+const path = require('path')
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
+
+// https://github.com/GreenFrogMCBE/Protocol/blob/master/src/datatypes/util.js#L34C1-L36C2
+function serialize (obj = {}, fmt) {
+  return JSON.stringify(obj, (k, v) => typeof v === 'bigint' ? v.toString() : v, fmt)
+}
 
 const relay = new Relay({
   version: args[0],
@@ -12,26 +17,28 @@ const relay = new Relay({
     host: args[3],
     port: parseInt(args[4]),
   }
-});
+})
 
-relay.listen();
+relay.listen()
 
 relay.on('connect', player => {
   player.on('clientbound', ({ name, params }) => {
-    if (name === 'move_entity' || name === 'set_entity_data' || name === 'level_event') return;
+    if (name === 'move_entity' || name === 'set_entity_data' || name === 'level_event') return
 
     const packet = {
       name,
       params,
-    };
-    const packetJson = JSON.stringify(packet);
-    const timestamp = Date.now();
-    const filename = `${name}_${timestamp}.json`;
-    const filePath = path.join(__dirname, filename);
-    fs.writeFile(filePath, packetJson, err => {
+    }
+
+    const packet_json = JSON.stringify(serialize(packet))
+    const timestamp = Date.now()
+    const file_name = `${name}_${timestamp}.json`
+    const file_path = path.join(__dirname, file_name)
+
+    fs.writeFile(file_path, packet_json, err => {
       if (err) {
-        console.error(err);
+        console.error(err)
       }
-    });
-  });
-});
+    })
+  })
+})
